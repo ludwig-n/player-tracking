85013145d21de406562c0418463caee4b1b6defe
Nick Ludwig <nick.ludwig.g@gmail.com>
2024-12-28 01:14:27 +0400

    Add logging to server

--- app/server/main.py
+++ app/server/main.py
@@ -1,3 +1,4 @@
+import logging.handlers
 import pathlib
 import zipfile
 
@@ -14,6 +15,9 @@ BOTSORT_CONFIG_PATH = pathlib.Path("config/botsort.yaml")
 RESULTS_DIR = pathlib.Path("results")
 IMAGES_DIR = RESULTS_DIR / "images"
 IMAGES_DIR.mkdir(parents=True, exist_ok=True)
+
+LOG_PATH = pathlib.Path("logs/server.log")
+LOG_PATH.parent.mkdir(exist_ok=True)
 
 ORIGINAL_PATH_NO_SUFFIX = RESULTS_DIR / "original"
 ANNOTATED_PATH = RESULTS_DIR / "annotated.mp4"
@@ -35,6 +39,8 @@ async def infer(video_file: fastapi.UploadFile):
       - ids of all detected players (player_ids),
       - time ranges when each player was present in the video (player_times).
     """
+    logging.info("Received POST /infer")
+
     original_path = ORIGINAL_PATH_NO_SUFFIX.with_suffix(
         pathlib.Path(video_file.filename).suffix
     )
@@ -72,6 +78,8 @@ async def infer(video_file: fastapi.UploadFile):
     app.state.boxes_list = boxes_list
     app.state.player_ids = player_ids
 
+    logging.info(f"/infer done, returning {ZIP_PATH}")
+
     return fastapi.responses.FileResponse(
         path=ZIP_PATH,
         headers={
@@ -87,44 +95,69 @@ async def make_video(player_params: dict[int, video.PlayerParams]):
     Generates a video with bounding boxes and labels like /infer,
     but using custom visualization parameters.
     """
+    logging.info(f"Received POST /make_video, player_params: {player_params}")
+
     if not hasattr(app.state, "player_ids"):
+        logging.warning("/make_video called without an /infer")
         raise fastapi.HTTPException(
             fastapi.status.HTTP_400_BAD_REQUEST, "/infer must be called first"
         )
+
     if set(player_params.keys()) != app.state.player_ids:
+        logging.warning(
+            "player_params has wrong keys: "
+            f"expected {sorted(app.state.player_ids)}, "
+            f"got {sorted(player_params.keys())}"
+        )
         raise fastapi.HTTPException(
             fastapi.status.HTTP_422_UNPROCESSABLE_ENTITY,
             "player_params must have exactly one element for each player id",
         )
+
     await video.draw_bboxes(
         app.state.original_path,
         ANNOTATED_PATH,
         app.state.boxes_list,
         player_params,
     )
+    logging.info(f"/make_video done, returning {ANNOTATED_PATH}")
     return ANNOTATED_PATH
 
 
 @app.post("/make_focused_video", response_class=fastapi.responses.FileResponse)
 async def make_focused_video(player_id: int):
     """Generates a video focused on a specific player's movements."""
+    logging.info(f"Received POST /make_focused_video, player_id: {player_id}")
+
     if not hasattr(app.state, "player_ids"):
+        logging.warning("/make_focused_video called without an /infer")
         raise fastapi.HTTPException(
             fastapi.status.HTTP_400_BAD_REQUEST, "/infer must be called first"
         )
+
     if player_id not in app.state.player_ids:
+        logging.warning(
+            f"player_id {player_id} not found in player ids "
+            f"{sorted(app.state.player_ids)}"
+        )
         raise fastapi.HTTPException(
             fastapi.status.HTTP_404_NOT_FOUND,
             f"player id {player_id} not found",
         )
+
     await video.crop_to_player(
         app.state.original_path,
         ANNOTATED_PATH,
         app.state.boxes_list,
         player_id,
     )
+    logging.info(f"/make_focused_video done, returning {ANNOTATED_PATH}")
     return ANNOTATED_PATH
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8500, reload=True)
+    log_handler = logging.handlers.TimedRotatingFileHandler(
+        filename=LOG_PATH, when="D", backupCount=7
+    )
+    logging.basicConfig(handlers=[log_handler], level=logging.INFO)
+    uvicorn.run("main:app", host="0.0.0.0", port=8500)
